c63b4b7c3761930c3f0994ad4c461b6be55ebcb2
Holger Mueller <9jholger@gmail.com>
2018-05-17 18:30:34 -0500

    worked on index

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,10 +1,12 @@
 //imports:
 //1-express
-
-
+const express = require('express');
 //2-burger.js
+const burger = require('./models/burger.js');
+//create router
+const router = express.Router();
 
-//create router 
+//create routes for MySQL logic
 
 
 //export router
