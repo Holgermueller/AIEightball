967ce9174241bbbd40cc346b687684501c920d03
Holger Mueller <9jholger@gmail.com>
2018-05-16 20:18:16 -0500

    got server running

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Create schema for burgers_db
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+    burger_id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(200),
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (burger_id)
+);
---
+++ server.js
@@ -0,0 +1,18 @@
+//require dependencies
+const express = require('express');
+const bodyparser = require('body-parser');
+const mysql = require('mysql');
+const exphb = require('express-handlebars');
+
+//create port
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+//leave space for other code
+
+
+
+//start server
+app.listen(PORT,function() {
+    console.log("Server listening on: http://localhost: " + PORT);
+});
