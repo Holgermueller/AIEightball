e22a5aef5993099d09c66a1e0739ae0b1d4516ab
Holger Mueller <9jholger@gmail.com>
2018-05-16 21:42:23 -0500

    populated db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 ###create burgers to populate database
-
+USE burgers_db;
 INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
