ddbbf2c005ad83faaec9881661230ba6e375e00c
Holger Mueller <9jholger@gmail.com>
2018-05-19 00:16:20 -0500

    got get request working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //require dependencies
 const express = require('express');
 const bodyparser = require('body-parser');
-const exphb = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 //remove later
 const mysql = require('mysql');
@@ -13,12 +13,35 @@ const app = express();
 
 //leave space for other code
 
-//test get request
-connection.query('SELECT * FROM burgers', function(err, res) {
-    if (err) throw err;
-    console.log(res);
-})
+//test mysql request
+// connection.query('SELECT * FROM burgers', function(err, res) {
+//     if (err) throw err;
+//     console.log(res);
+// })
 
+//test data
+let burgers = [
+    {
+        burger: "cheeseburger"
+    },
+    {
+        burger: "California Burger"
+    },
+    {
+        burger: "bacon cheeseburger"
+    }
+];
+
+//test get request
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+    app.get("/", function (req, res) {
+      res.render("index", {index: burgers});
+    });
+
+//end remove
 
 //start server
 app.listen(PORT, function () {
