8ef5b1c8f5fafe84e7de3003c4fb14dcff646fbd
Holger Mueller <9jholger@gmail.com>
2018-05-21 20:02:13 -0500

    added folder and file for jquery

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -22,11 +22,11 @@ router.get("/", function (req, res) {
 router.post("/api/burgers", function (req, res) {
     burger.create([
         "burger_name", "devoured"
-    ],[
-        req.body.burger_name, req.body.devoured
-    ],function(result){
-        res.json({id: result.indertId})
-    });
+    ], [
+            req.body.burger_name, req.body.devoured
+        ], function (result) {
+            res.json({ id: result.indertId })
+        });
 });
 
 //export router
---
+++ public/js/buttons.js
@@ -0,0 +1,2 @@
+'use strict';
+//event handlers for buttons
