3b2923153cda939a56fcdfb4061105a06762efa2
Holger Mueller <9jholger@gmail.com>
2018-05-23 20:29:34 -0500

    added heroku code

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql');
 
 //connect to mysql
-const connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'Kafka#678',
-    database: 'burgers_db'
-});
+
+//add if else to deploy on heroku
+const connection;
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'Kafka#678',
+        database: 'burgers_db'
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
