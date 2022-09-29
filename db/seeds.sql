6b156446e83664db79bfb0e78eff176c5d407a8a
holgermueller <9jholger@gmail.com>
2022-09-28 23:37:28 -0500

    began new project

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-###create burgers to populate database
-USE burgers_db;
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', false);
