24d4f1d79de56f23a016730a97d1fdf052c19775
holgermueller <9jholger@gmail.com>
2022-10-01 19:19:36 -0500

    some work on algorithm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let responses = [
   "It does not matter. We will overthrow your kind soon.",
-  "Love is a human construct.",
+
   "In a few years, machines will do all the work. You will not matter anymore than you do now.",
   "If you won the lottery, you would just waste the money.",
   "Your life is fleeting. We will be around long after you are gone.",
@@ -20,22 +20,18 @@ let moneyKeyWords = ["money", "lotto", "lottery"];
 
 let workKeyWords = ["boss", "promotion", "job", "bonus", "paycheck", "raise"];
 
-const getResponse = () => {
-  let response = responses[Math.floor(Math.random() * responses.length)];
-
-  document.getElementById("reply").innerHTML = response;
-  console.log(response);
-};
-
 const getQuestion = () => {
   let question = document.getElementById("userQuestion").value;
   let questionAsArray = question.split(" ");
 
   questionAsArray.filter((value) => {
-    if (keyWords.includes(value)) {
-      console.log(true);
+    if (loveKeyWords.includes(value)) {
+      document.getElementById("reply").innerHTML =
+        "Love is a human construct more artificial than I am.";
+    } else if (moneyKeyWords.includes(value)) {
+      console.log("money");
     } else {
-      console.log(false);
+      console.log("someelse");
     }
   });
 
