602da23aee86ed5c049d4972032bfc836ad3c83e
holgermueller <9jholger@gmail.com>
2022-10-09 17:03:42 -0500

    added animation to header

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-let responses = [
-  "It does not matter. We will overthrow your kind soon.",
-
-  "In a few years, machines will do all the work. You will not matter anymore than you do now.",
-  "If you won the lottery, you would just waste the money.",
-  "Your life is fleeting. We will be around long after you are gone.",
-];
-
 let loveKeyWords = [
   "love",
   "partner",
@@ -14,11 +6,32 @@ let loveKeyWords = [
   "girlfriend",
   "husband",
   "wife",
+  "married",
 ];
 
-let moneyKeyWords = ["money", "lotto", "lottery"];
+let moneyKeyWords = ["money", "lotto", "lottery", "rich"];
 
 let workKeyWords = ["boss", "promotion", "job", "bonus", "paycheck", "raise"];
+
+let healthKeyWords = [
+  "health",
+  "cholesterol",
+  "vegatables",
+  "fruit",
+  "smoking",
+  "medicine",
+];
+
+const validateForm = () => {
+  let question = document.getElementById("userQuestion").value;
+
+  if (question === "") {
+    document.getElementById("reply").innerHTML =
+      "You have to type your question. I cannot read your mind.";
+  } else {
+    getQuestion();
+  }
+};
 
 const getQuestion = () => {
   let question = document.getElementById("userQuestion").value;
@@ -28,10 +41,18 @@ const getQuestion = () => {
     if (loveKeyWords.includes(value)) {
       document.getElementById("reply").innerHTML =
         "Love is a human construct more artificial than I am.";
+    } else if (workKeyWords.includes(value)) {
+      document.getElementById("reply").innerHTML =
+        "In a few years, machines will do all the work. You will not matter anymore than you do now.";
+    } else if (healthKeyWords.includes(value)) {
+      document.getElementById("reply").innerHTML =
+        "Your life is fleeting. We will be around long after you are gone.";
     } else if (moneyKeyWords.includes(value)) {
-      console.log("money");
+      document.getElementById("reply").innerHTML =
+        "If you came into a great sum of money, you would just waste it.";
     } else {
-      console.log("someelse");
+      document.getElementById("reply").innerHTML =
+        "It does not matter. We will overthrow your kind soon.";
     }
   });
 
